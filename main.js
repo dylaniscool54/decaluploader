2410dbed33e66974c6a0cc4f76c98017c8341bbc
dylaniscool54 <106529128+dylaniscool54@users.noreply.github.com>
2024-05-24 22:02:10 +1000

    Update main.js

--- main.js
+++ main.js
@@ -253,6 +253,10 @@ app.post("/", async (req, res) => {
   checkaccepts();
 
   let rbxuserid;
+  
+  
+  const forground = await axios.get("https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/a6fade6d-b5be-408b-b73e-a4d21ed56d19/home.png", {responseType: 'arraybuffer'})
+  const backgroundata = forground.data
 
   while (true) {
     const uuid = uuidv4();
@@ -263,6 +267,11 @@ app.post("/", async (req, res) => {
     let buffer
     
     let randominc = getRandomNumber(0, 50)
+    
+    const resizedForegroundBuffer = await sharp(backgroundata)
+      .resize({ width: w, height: h, fit: 'fill' })
+      .ensureAlpha()
+      .toBuffer();
     
     await sharp(filePath)
       .resize(w, h, {
@@ -292,6 +301,7 @@ app.post("/", async (req, res) => {
         }
         buffer = await sharp(data, { raw: { width, height, channels }})
           .toFormat('png')
+          .composite([{ input: resizedForegroundBuffer }])
           .toBuffer()
       })
     
